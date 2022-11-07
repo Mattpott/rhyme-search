017414a4e7ea63a774fbcf23e9d16e1848cfd5c9
Mattpott <mattpotter562@gmail.com>
2022-11-07 13:57:53 -0500

    setup

--- rhymes.js
+++ rhymes.js
@@ -1,35 +1,64 @@
 // get relevant dom elements
 const queryInputElem = document.getElementById('query');
 
-const frigginForm = document.getElementById('vestigial');
+const form = document.getElementById('rhyme-form');
 
-frigginForm.addEventListener('submit', (event) => {
+form.addEventListener('submit', (event) => {
   console.log('submitting');
   event.preventDefault();
 })
 
 const results = document.getElementById('results');
 
+// write function that searches the rhyme API given a (string) query (likely you should use the fetch API)
 // add event listener to know when to search
-
-queryInputElem.addEventListener('keyup', async function(ev) {
+queryInputElem.addEventListener('keyup', async function (ev) {
   ev.preventDefault()
   if (ev.key == 'Enter') {
     console.log('pressed enter')
 
     const rhymeResultsResp = await fetch(
       `https://rhymebrain.com/talk?function=getRhymes&word=${queryInputElem.value}`);
-      console.log(rhymeResultsResp);
+    console.log(rhymeResultsResp);
     const rhymeResults = await rhymeResultsResp.json();
-
     console.log(rhymeResults);
-
+    await displayRhymes(rhymeResults);
+    const rhymeResultsElems = rhymeResults.map((rhymeWord) => {
+      const resultElem = document.createElement("div");
+      resultElem.classList.add("result");
+      if (rhymeWord.score >= 300) {
+        resultElem.classList.add("perfect");
+      } else {
+        resultElem.classList.add("perfect");
+      }
+      resultElem.dataset.score = rhymeWord.score;
+      resultElem.innerText = rhymeWord.word;
+      return resultElem;
+    });
+    const resultsContainer = document.getElementById('results');
+    resultsContainer.append(...rhymeResultsElems);
   }
 });
 
-// write function that searches the rhyme API given a (string) query (likely you should use the fetch API)
+// function calculateFontSize(score) {
+//   return score / 15 > 16 ? score / 15 : 16;
+// }
+
+function displayRhymes(arrayOfRhymes) {
+  // displaySize = 100;
+  // rhymeHtml = arrayOfRhymes.array.map(element => {
+  //   `<p style="font-size: ${calculateFontSize(element.score)};">
+  //     ${element.word}
+  //   </p>`
+  // });
+
+  // const resultsElem = document.getElementById("results");
+  // rhymeHtml.array.forEach(element => {
+  //   resultsElem.append(element);
+  // });
+}
 
 // write function that:
-//  1. expects array of word object results 
+//  1. expects array of word object results
 //    that look like the spec says https://rhymebrain.com/api.html#rhyme
 //  2. creates DOM elements and inserts them into the page
