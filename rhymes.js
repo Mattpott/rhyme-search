48cd7a89fe7189e21c09e9757d552b46e4ab9842
Mattpott <mattpotter562@gmail.com>
2022-11-07 15:13:42 -0500

    Finished

--- rhymes.js
+++ rhymes.js
@@ -17,48 +17,38 @@ queryInputElem.addEventListener('keyup', async function (ev) {
   if (ev.key == 'Enter') {
     console.log('pressed enter')
 
+
     const rhymeResultsResp = await fetch(
       `https://rhymebrain.com/talk?function=getRhymes&word=${queryInputElem.value}`);
     console.log(rhymeResultsResp);
     const rhymeResults = await rhymeResultsResp.json();
     console.log(rhymeResults);
-    await displayRhymes(rhymeResults);
-    const rhymeResultsElems = rhymeResults.map((rhymeWord) => {
-      const resultElem = document.createElement("div");
-      resultElem.classList.add("result");
-      if (rhymeWord.score >= 300) {
-        resultElem.classList.add("perfect");
-      } else {
-        resultElem.classList.add("perfect");
-      }
-      resultElem.dataset.score = rhymeWord.score;
-      resultElem.innerText = rhymeWord.word;
-      return resultElem;
-    });
-    const resultsContainer = document.getElementById('results');
-    resultsContainer.append(...rhymeResultsElems);
+    displayRhymes(rhymeResults);
   }
 });
-
-// function calculateFontSize(score) {
-//   return score / 15 > 16 ? score / 15 : 16;
-// }
-
-function displayRhymes(arrayOfRhymes) {
-  // displaySize = 100;
-  // rhymeHtml = arrayOfRhymes.array.map(element => {
-  //   `<p style="font-size: ${calculateFontSize(element.score)};">
-  //     ${element.word}
-  //   </p>`
-  // });
-
-  // const resultsElem = document.getElementById("results");
-  // rhymeHtml.array.forEach(element => {
-  //   resultsElem.append(element);
-  // });
-}
 
 // write function that:
 //  1. expects array of word object results
 //    that look like the spec says https://rhymebrain.com/api.html#rhyme
 //  2. creates DOM elements and inserts them into the page
+function displayRhymes(rhymeResults) {
+  const resultsContainer = document.getElementById('results');
+  while (resultsContainer.hasChildNodes()) {
+    resultsContainer.removeChild(resultsContainer.lastChild);
+  }
+  const rhymeResultsElems = rhymeResults.map((rhymeWord) => {
+    const resultElem = document.createElement("div");
+    resultElem.classList.add("result");
+    resultElem.style.fontSize = `${scaleFontSize(rhymeWord.score)}px`;
+    resultElem.dataset.score = rhymeWord.score;
+    resultElem.innerText = rhymeWord.word;
+    resultElem.style.padding = "0.5rem";
+    resultElem.style.display = "inline-block";
+    return resultElem;
+  });
+  resultsContainer.append(...rhymeResultsElems);
+}
+
+function scaleFontSize(score) {
+  return 50 * (score / 300);
+}
